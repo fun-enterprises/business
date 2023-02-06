6744c9e913251bedcb1529b0832af422cf438802
Santiago De Simone <santidesimone@Santiagos-MacBook-Pro.local>
2023-02-06 20:43:04 -0300

    fix config lang

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -40,8 +40,8 @@ export default {
             nav: nav('en'),
         
             sidebar: {
-              '/en/guide/': sidebarGuide(),
-              '/en/config/': sidebarConfig()
+              '/en/guide/': sidebarGuide('en'),
+              '/en/config/': sidebarConfig('en')
             },
         
             editLink: {
@@ -88,8 +88,8 @@ export default {
             nav: nav('es'),
         
             sidebar: {
-              '/es/guide/': sidebarGuide(),
-              '/es/config/': sidebarConfig()
+              '/es/guide/': sidebarGuide('es'),
+              '/es/config/': sidebarConfig('es')
             },
         
             editLink: {
@@ -127,8 +127,8 @@ export default {
                 nav: nav('it'),
             
                 sidebar: {
-                  '/it/guide/': sidebarGuide(),
-                  '/it/config/': sidebarConfig()
+                  '/it/guide/': sidebarGuide('it'),
+                  '/it/config/': sidebarConfig('it')
                 },
             
                 editLink: {
@@ -224,78 +224,231 @@ function nav(lang) {
 
   }
   
-  function sidebarGuide() {
-    return [
-      {
-        text: 'Introduction',
-        collapsed: false,
-        items: [
-          { text: 'What is VitePress?', link: '/guide/what-is-vitepress' },
-          { text: 'Getting Started', link: '/guide/getting-started' },
-          { text: 'Configuration', link: '/guide/configuration' },
-          { text: 'Routing', link: '/guide/routing' },
-          { text: 'Deploying', link: '/guide/deploying' },
-          { text: 'Internationalization', link: '/guide/i18n' }
-        ]
-      },
-      {
-        text: 'Writing',
-        collapsed: false,
-        items: [
-          { text: 'Markdown', link: '/guide/markdown' },
-          { text: 'Asset Handling', link: '/guide/asset-handling' },
-          { text: 'Frontmatter', link: '/guide/frontmatter' },
-          { text: 'Using Vue in Markdown', link: '/guide/using-vue' },
-          { text: 'API Reference', link: '/guide/api' }
-        ]
-      },
-      {
-        text: 'Theme',
-        collapsed: false,
-        items: [
-          { text: 'Introduction', link: '/guide/theme-introduction' },
-          { text: 'Nav', link: '/guide/theme-nav' },
-          { text: 'Sidebar', link: '/guide/theme-sidebar' },
-          { text: 'Prev Next Link', link: '/guide/theme-prev-next-link' },
-          { text: 'Edit Link', link: '/guide/theme-edit-link' },
-          { text: 'Last Updated', link: '/guide/theme-last-updated' },
-          { text: 'Layout', link: '/guide/theme-layout' },
-          { text: 'Home Page', link: '/guide/theme-home-page' },
-          { text: 'Team Page', link: '/guide/theme-team-page' },
-          { text: 'Badge', link: '/guide/theme-badge' },
-          { text: 'Footer', link: '/guide/theme-footer' },
-          { text: 'Search', link: '/guide/theme-search' },
-          { text: 'Carbon Ads', link: '/guide/theme-carbon-ads' }
-        ]
-      },
-      {
-        text: 'Migrations',
-        collapsed: false,
-        items: [
-          {
-            text: 'Migration from VuePress',
-            link: '/guide/migration-from-vuepress'
-          },
-          {
-            text: 'Migration from VitePress 0.x',
-            link: '/guide/migration-from-vitepress-0'
-          }
-        ]
-      }
-    ]
+  function sidebarGuide(lang) {
+    if (lang=='it'){
+        return [
+            {
+              text: 'Introduction',
+              collapsed: false,
+              items: [
+                { text: 'What is VitePress?', link: '/it/guide/what-is-vitepress' },
+                { text: 'Getting Started', link: '/it/guide/getting-started' },
+                { text: 'Configuration', link: '/it/guide/configuration' },
+                { text: 'Routing', link: '/it/guide/routing' },
+                { text: 'Deploying', link: '/it/guide/deploying' },
+                { text: 'Internationalization', link: '/it/guide/i18n' }
+              ]
+            },
+            {
+              text: 'Writing',
+              collapsed: false,
+              items: [
+                { text: 'Markdown', link: '/it/guide/markdown' },
+                { text: 'Asset Handling', link: '/it/guide/asset-handling' },
+                { text: 'Frontmatter', link: '/it/guide/frontmatter' },
+                { text: 'Using Vue in Markdown', link: '/it/guide/using-vue' },
+                { text: 'API Reference', link: '/it/guide/api' }
+              ]
+            },
+            {
+              text: 'Theme',
+              collapsed: false,
+              items: [
+                { text: 'Introduction', link: '/it/guide/theme-introduction' },
+                { text: 'Nav', link: '/it/guide/theme-nav' },
+                { text: 'Sidebar', link: '/it/guide/theme-sidebar' },
+                { text: 'Prev Next Link', link: '/it/guide/theme-prev-next-link' },
+                { text: 'Edit Link', link: '/it/guide/theme-edit-link' },
+                { text: 'Last Updated', link: '/it/guide/theme-last-updated' },
+                { text: 'Layout', link: '/it/guide/theme-layout' },
+                { text: 'Home Page', link: '/it/guide/theme-home-page' },
+                { text: 'Team Page', link: '/it/guide/theme-team-page' },
+                { text: 'Badge', link: '/it/guide/theme-badge' },
+                { text: 'Footer', link: '/it/guide/theme-footer' },
+                { text: 'Search', link: '/it/guide/theme-search' },
+                { text: 'Carbon Ads', link: '/it/guide/theme-carbon-ads' }
+              ]
+            },
+            {
+              text: 'Migrations',
+              collapsed: false,
+              items: [
+                {
+                  text: 'Migration from VuePress',
+                  link: '/it/guide/migration-from-vuepress'
+                },
+                {
+                  text: 'Migration from VitePress 0.x',
+                  link: '/it/guide/migration-from-vitepress-0'
+                }
+              ]
+            }
+          ]
+    }
+    if (lang=='es'){
+        return [
+            {
+              text: 'Introduction',
+              collapsed: false,
+              items: [
+                { text: 'What is VitePress?', link: '/es/guide/what-is-vitepress' },
+                { text: 'Getting Started', link: '/es/guide/getting-started' },
+                { text: 'Configuration', link: '/es/guide/configuration' },
+                { text: 'Routing', link: '/es/guide/routing' },
+                { text: 'Deploying', link: '/es/guide/deploying' },
+                { text: 'Internationalization', link: '/es/guide/i18n' }
+              ]
+            },
+            {
+              text: 'Writing',
+              collapsed: false,
+              items: [
+                { text: 'Markdown', link: '/es/guide/markdown' },
+                { text: 'Asset Handling', link: '/es/guide/asset-handling' },
+                { text: 'Frontmatter', link: '/es/guide/frontmatter' },
+                { text: 'Using Vue in Markdown', link: '/es/guide/using-vue' },
+                { text: 'API Reference', link: '/es/guide/api' }
+              ]
+            },
+            {
+              text: 'Theme',
+              collapsed: false,
+              items: [
+                { text: 'Introduction', link: '/es/guide/theme-introduction' },
+                { text: 'Nav', link: '/es/guide/theme-nav' },
+                { text: 'Sidebar', link: '/es/guide/theme-sidebar' },
+                { text: 'Prev Next Link', link: '/es/guide/theme-prev-next-link' },
+                { text: 'Edit Link', link: '/es/guide/theme-edit-link' },
+                { text: 'Last Updated', link: '/es/guide/theme-last-updated' },
+                { text: 'Layout', link: '/es/guide/theme-layout' },
+                { text: 'Home Page', link: '/es/guide/theme-home-page' },
+                { text: 'Team Page', link: '/es/guide/theme-team-page' },
+                { text: 'Badge', link: '/es/guide/theme-badge' },
+                { text: 'Footer', link: '/es/guide/theme-footer' },
+                { text: 'Search', link: '/es/guide/theme-search' },
+                { text: 'Carbon Ads', link: '/es/guide/theme-carbon-ads' }
+              ]
+            },
+            {
+              text: 'Migrations',
+              collapsed: false,
+              items: [
+                {
+                  text: 'Migration from VuePress',
+                  link: '/es/guide/migration-from-vuepress'
+                },
+                {
+                  text: 'Migration from VitePress 0.x',
+                  link: '/es/guide/migration-from-vitepress-0'
+                }
+              ]
+            }
+          ]
+        
+    }
+    if (lang=='en'){
+        return [
+            {
+              text: 'Introduction',
+              collapsed: false,
+              items: [
+                { text: 'What is VitePress?', link: '/en/guide/what-is-vitepress' },
+                { text: 'Getting Started', link: '/en/guide/getting-started' },
+                { text: 'Configuration', link: '/en/guide/configuration' },
+                { text: 'Routing', link: '/en/guide/routing' },
+                { text: 'Deploying', link: '/en/guide/deploying' },
+                { text: 'Internationalization', link: '/en/guide/i18n' }
+              ]
+            },
+            {
+              text: 'Writing',
+              collapsed: false,
+              items: [
+                { text: 'Markdown', link: '/en/guide/markdown' },
+                { text: 'Asset Handling', link: '/en/guide/asset-handling' },
+                { text: 'Frontmatter', link: '/en/guide/frontmatter' },
+                { text: 'Using Vue in Markdown', link: '/en/guide/using-vue' },
+                { text: 'API Reference', link: '/en/guide/api' }
+              ]
+            },
+            {
+              text: 'Theme',
+              collapsed: false,
+              items: [
+                { text: 'Introduction', link: '/en/guide/theme-introduction' },
+                { text: 'Nav', link: '/en/guide/theme-nav' },
+                { text: 'Sidebar', link: '/en/guide/theme-sidebar' },
+                { text: 'Prev Next Link', link: '/en/guide/theme-prev-next-link' },
+                { text: 'Edit Link', link: '/en/guide/theme-edit-link' },
+                { text: 'Last Updated', link: '/en/guide/theme-last-updated' },
+                { text: 'Layout', link: '/en/guide/theme-layout' },
+                { text: 'Home Page', link: '/en/guide/theme-home-page' },
+                { text: 'Team Page', link: '/en/guide/theme-team-page' },
+                { text: 'Badge', link: '/en/guide/theme-badge' },
+                { text: 'Footer', link: '/en/guide/theme-footer' },
+                { text: 'Search', link: '/en/guide/theme-search' },
+                { text: 'Carbon Ads', link: '/en/guide/theme-carbon-ads' }
+              ]
+            },
+            {
+              text: 'Migrations',
+              collapsed: false,
+              items: [
+                {
+                  text: 'Migration from VuePress',
+                  link: '/en/guide/migration-from-vuepress'
+                },
+                {
+                  text: 'Migration from VitePress 0.x',
+                  link: '/en/guide/migration-from-vitepress-0'
+                }
+              ]
+            }
+          ]
+        
+    }
   }
   
-  function sidebarConfig() {
-    return [
-      {
-        text: 'Config',
-        items: [
-          { text: 'Introduction', link: '/config/introduction' },
-          { text: 'App Configs', link: '/config/app-configs' },
-          { text: 'Theme Configs', link: '/config/theme-configs' },
-          { text: 'Frontmatter Configs', link: '/config/frontmatter-configs' }
-        ]
-      }
-    ]
+  function sidebarConfig(lang) {
+    if (lang=='it'){
+        return [
+            {
+              text: 'Config',
+              items: [
+                { text: 'Introduction', link: '/it/config/introduction' },
+                { text: 'App Configs', link: '/it/config/app-configs' },
+                { text: 'Theme Configs', link: '/it/config/theme-configs' },
+                { text: 'Frontmatter Configs', link: '/it/config/frontmatter-configs' }
+              ]
+            }
+          ]
+    }
+    if (lang=='es'){
+        return [
+            {
+              text: 'Config',
+              items: [
+                { text: 'Introduction', link: '/es/config/introduction' },
+                { text: 'App Configs', link: '/es/config/app-configs' },
+                { text: 'Theme Configs', link: '/es/config/theme-configs' },
+                { text: 'Frontmatter Configs', link: '/es/config/frontmatter-configs' }
+              ]
+            }
+          ]
+    }
+    if (lang=='en'){
+        return [
+            {
+              text: 'Config',
+              items: [
+                { text: 'Introduction', link: '/en/config/introduction' },
+                { text: 'App Configs', link: '/en/config/app-configs' },
+                { text: 'Theme Configs', link: '/en/config/theme-configs' },
+                { text: 'Frontmatter Configs', link: '/en/config/frontmatter-configs' }
+              ]
+            }
+          ]
+    }
+
   }
   
